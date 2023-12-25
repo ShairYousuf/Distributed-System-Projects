a3233b5ff379113805126e347c593f0414d93bc2
ShairYousuf <syjahin@mun.ca>
2023-12-25 14:44:45 -0330

    Added Webcrawler and renamed folder

---
+++ A Tour of go/webCrawler.go
@@ -0,0 +1,124 @@
+package main
+
+import (
+	"fmt"
+	"sync"
+)
+
+type Fetcher interface {
+	// Fetch returns the body of URL and
+	// a slice of URLs found on that page.
+	Fetch(url string) (body string, urls []string, err error)
+}
+// SafeCache is a safe concurrent map for caching fetched URLs.
+type SafeCache struct {
+	mu    sync.Mutex
+	cache map[string]bool
+}
+
+func (c *SafeCache) MarkVisited(url string) {
+	c.mu.Lock()
+	c.cache[url] = true
+	c.mu.Unlock()
+}
+
+func (c *SafeCache) WasVisited(url string) bool {
+	c.mu.Lock()
+	defer c.mu.Unlock()
+	return c.cache[url]
+}
+
+var cache = SafeCache{cache: make(map[string]bool)}
+
+// Crawl uses fetcher to recursively crawl
+// pages starting with url, to a maximum of depth.
+func Crawl(url string, depth int, fetcher Fetcher) {
+	// Check if the URL has already been visited.
+	if cache.WasVisited(url) {
+		return
+	}
+
+	// Mark the URL as visited to avoid fetching it again.
+	cache.MarkVisited(url)
+
+	if depth <= 0 {
+		return
+	}
+
+	body, urls, err := fetcher.Fetch(url)
+	if err != nil {
+		fmt.Println(err)
+		return
+	}
+
+	fmt.Printf("found: %s %q\n", url, body)
+
+	// Used a WaitGroup to wait for all goroutines to finish.
+	var wg sync.WaitGroup
+
+	// Fetch URLs in parallel.
+	for _, u := range urls {
+		wg.Add(1)
+		go func(u string) {
+			defer wg.Done()
+			Crawl(u, depth-1, fetcher)
+		}(u)
+	}
+
+	// Wait for all goroutines to finish before returning.
+	wg.Wait()
+	return
+}
+
+func main() {
+	Crawl("https://golang.org/", 4, fetcher)
+}
+
+// fakeFetcher is Fetcher that returns canned results.
+type fakeFetcher map[string]*fakeResult
+
+type fakeResult struct {
+	body string
+	urls []string
+}
+
+func (f fakeFetcher) Fetch(url string) (string, []string, error) {
+	if res, ok := f[url]; ok {
+		return res.body, res.urls, nil
+	}
+	return "", nil, fmt.Errorf("not found: %s", url)
+}
+
+// fetcher is a populated fakeFetcher.
+var fetcher = fakeFetcher{
+	"https://golang.org/": &fakeResult{
+		"The Go Programming Language",
+		[]string{
+			"https://golang.org/pkg/",
+			"https://golang.org/cmd/",
+		},
+	},
+	"https://golang.org/pkg/": &fakeResult{
+		"Packages",
+		[]string{
+			"https://golang.org/",
+			"https://golang.org/cmd/",
+			"https://golang.org/pkg/fmt/",
+			"https://golang.org/pkg/os/",
+		},
+	},
+	"https://golang.org/pkg/fmt/": &fakeResult{
+		"Package fmt",
+		[]string{
+			"https://golang.org/",
+			"https://golang.org/pkg/",
+		},
+	},
+	"https://golang.org/pkg/os/": &fakeResult{
+		"Package os",
+		[]string{
+			"https://golang.org/",
+			"https://golang.org/pkg/",
+		},
+	},
+}
