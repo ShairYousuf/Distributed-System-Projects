274cf0828e27e1af25edbf9044a0c8d333bbb389
shairyousuf <syjahin@mun.ca>
2023-12-29 06:56:10 -0330

    Coded the master/coordinator to handle assigntasks

--- MapReduce/6.5840/src/mr/coordinator.go
+++ MapReduce/6.5840/src/mr/coordinator.go
@@ -1,29 +1,93 @@
 package mr
 
-import "log"
-import "net"
-import "os"
-import "net/rpc"
-import "net/http"
+import (
+	"fmt"
+	"log"
+	"net"
+	"net/http"
+	"net/rpc"
+	"os"
+	"sync"
+	"time"
+)
+
+const (
+	IDLE = 0
+	IN_PROGRESS = 1
+	COMPLETED = 2
+)
+
+type Task struct{
+	Status int // this is kept as string in guide
+	WorkerId string
+	StartedAt time.Time
+}
+
+type MapTask struct{
+	FileName string
+	NReduce int
+	Task
+}
+
+type ReduceTask struct{
+	Region int
+	Locations []string
+	Task
+}
 
 
 type Coordinator struct {
-	// Your definitions here.
+	MapTasks []*MapTask
+	MapTasksRemaining int
+	ReduceTasks []*ReduceTask
+	ReduceTasksRemaining int
+	Mu sync.Mutex
 
 }
 
-// Your code here -- RPC handlers for the worker to call.
+type MapReduceTask interface{
+	AssignTask(workerId string)
+}
 
-//
-// an example RPC handler.
-//
-// the RPC argument and reply types are defined in rpc.go.
-//
-func (c *Coordinator) Example(args *ExampleArgs, reply *ExampleReply) error {
-	reply.Y = args.X + 1
+func(t*Task) AssignTask(workerId string){
+	t.Status = IN_PROGRESS
+	t.WorkerId = workerId
+	t.StartedAt = time.Now()
+}
+
+// Finds the first idle map task then first idle reduce task.
+// If task found, then update the task with the worker assignment
+// If no tasks, then reply is set to nil
+func (c * Coordinator) AssignTask(workerId string, task *MapReduceTask) error {
+	c.Mu.Lock()
+	defer c.Mu.Unlock()
+
+	if c.MapTasksRemaining !=0 {
+		for _, mt:= range c.MapTasks{
+			if mt.Status == IDLE {
+				fmt.Printf("[Coordingator] Assigning Map Task: %v\n", mt.FileName)
+				mt.AssignTask(workerId)
+				*task = mt
+				break
+			}
+		}
+		return nil
+	}
+	if c.ReduceTasksRemaining!=0{
+		for _, rt:= range c.ReduceTasks{
+			if rt.Status ==IDLE{
+				fmt.Printf("[Coordinator] Assigning Reduce Task: %v \n", rt.Region)
+				rt.AssignTask(workerId)
+				*task = rt
+				break
+			}
+		}
+		return nil
+	}
+	fmt.Println("[Coordinator] No idle tasks found")
+	task = nil
 	return nil
 }
-
 
 //
 // start a thread that listens for RPCs from worker.go
@@ -48,8 +112,9 @@ func (c *Coordinator) server() {
 func (c *Coordinator) Done() bool {
 	ret := false
 
-	// Your code here.
-
+	if c.MapTasksRemaining == 0 && c.ReduceTasksRemaining == 0{
+		ret = true
+	}
 
 	return ret
 }
@@ -61,9 +126,19 @@ func (c *Coordinator) Done() bool {
 //
 func MakeCoordinator(files []string, nReduce int) *Coordinator {
 	c := Coordinator{}
+	c.MapTasks = make([]*MapTask,len(files))
+	c.MapTasksRemaining = len(files)
+	c.ReduceTasks = make([]*ReduceTask,nReduce)
+	c.ReduceTasksRemaining = nReduce
+	c.Mu = sync.Mutex{}
 
-	// Your code here.
-
+	for i, file := range files{
+		c.MapTasks[i] = &MapTask{
+			FileName: file,
+			NReduce: nReduce,
+			Task: Task{Status: IDLE},
+		}
+	}
 
 	c.server()
 	return &c
